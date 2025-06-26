9d7b81a23009c943b5ac60ab3faf765e1f00b70b
Bugo <cheetah@bk.ru>
2025-06-26 21:32:42 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -140,7 +140,16 @@ export default defineConfig({
       ],
       title: 'htmx по-русски',
       description: 'Документация htmx на русском языке.',
-      head: [],
+      head: [
+        {
+          tag: 'script',
+          content: `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "s5m0d6cnjz");`,
+        },
+      ],
       defaultLocale: 'root',
       locales: {
         root: {
